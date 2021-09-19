1d3155597698d0f44c47adf056847da746008a0c
Laurie Waller <lauriewaller7@gmail.com>
2021-09-18 22:21:45 -0700

    create working app

--- src/Headlines.js
+++ src/Headlines.js
@@ -1,44 +1,23 @@
 import React from "react";
+import { connect } from "react-redux";
+import { makeApiCall } from "./actions";
 
 class Headlines extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      headlines: [],
-    };
   }
-
-  makeApiCall = () => {
-    fetch(
-      `https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${process.env.REACT_APP_API_KEY}`
-    )
-      .then((response) => response.json())
-      .then((jsonifiedResponse) => {
-        this.setState({
-          isLoaded: true,
-          headlines: jsonifiedResponse.results,
-        });
-      })
-      .catch((error) => {
-        this.setState({
-          isLoaded: true,
-          error,
-        });
-      });
-  };
-
   componentDidMount() {
-    this.makeApiCall();
+    // Now we'll use dispatch() to make our API call.
+    const { dispatch } = this.props;
+    dispatch(makeApiCall());
   }
   // Of course, there is a downside to this - the potential to hit API limits very quickly. Any time the component is reloaded, another call will be made. If you're using this approach to make calls to an API that has strict limits, we recommend being careful about having a local server running while you're working - because every time you make a change to your code, it'll trigger a component reload and another API call.
 
   render() {
-    const { error, isLoaded, headlines } = this.state;
+    const { error, isLoading, headlines } = this.props;
     if (error) {
       return <React.Fragment>Error: {error.message}</React.Fragment>;
-    } else if (!isLoaded) {
+    } else if (isLoading) {
       return <React.Fragment>Loading...</React.Fragment>;
     } else {
       return (
@@ -58,6 +37,14 @@ class Headlines extends React.Component {
   }
 }
 
-export default Headlines;
+const mapStateToProps = (state) => {
+  return {
+    headlines: state.headlines,
+    isLoading: state.isLoading,
+    error: state.error,
+  };
+};
 
+export default connect(mapStateToProps)(Headlines);
+// This will allow us to wrap our component with an HOC that provides Redux functionality such as the dispatch() function.
 // Our component will mount before we make an API call. We'll put the code for the API call in the componentDidMount lifecycle method so it runs as soon as our component is mounted. Once the call is complete, isLoaded will be switched to true.
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reducer from "./reducers/headlines-reducer";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunkMiddleware from "redux-thunk";
+import middlewareLogger from "./middleware/middleware-logger";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const store = createStore(
+  reducer,
+  applyMiddleware(thunkMiddleware, middlewareLogger)
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
